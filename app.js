17df2e411eb49fe6dc9bbc2ab0bd1aa942c84812
khuseynkhasiev <khuseyn-khasiev@mail.ru>
2023-03-07 15:45:33 +0300

    refactor: unnecessary dependencies have been removed and code refactoring has been done in app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const { ERROR_NOT_FOUND } = require('./errors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(path.join((__dirname, 'public'))));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
     _id: '6403823bc422b225a6584a02',
@@ -19,7 +17,7 @@ app.use((req, res, next) => {
 });
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
-app.use('*', (req, res) => res.status(404).send({ message: '404 Not Found' }));
+app.use('*', (req, res) => res.status(ERROR_NOT_FOUND).send({ message: `${ERROR_NOT_FOUND} Not Found` }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
