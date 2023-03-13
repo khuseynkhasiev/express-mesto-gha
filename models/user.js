a62afe51d2c87e3506d3edf2e9a4a4b89471c9f5
khuseynkhasiev <khuseyn-khasiev@mail.ru>
2023-03-13 21:38:18 +0300

    feat: an email and password have been added to the user's schema, mandatory fields have been changed and default values have been set

--- models/user.js
+++ models/user.js
@@ -2,20 +2,28 @@ const mongoose = require('mongoose');
 
 const user = new mongoose.Schema({
   name: {
-    required: true,
     type: String,
     minLength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
-    required: true,
     type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
     required: true,
     type: String,
+    unique: true,
+  },
+  password: {
+    required: true,
   },
 });
 
