bb23be1b2748a627d8b16713913fc010c25bbc77
khuseynkhasiev <khuseyn-khasiev@mail.ru>
2023-03-14 20:42:06 +0300

    feat: added a 401 error constant, created a login verification controller, added a hash and salt when creating a user to the controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const {
   ERROR_INACCURATE_DATA,
   ERROR_NOT_FOUND,
   ERROR_INTERNAL_SERVER,
+  ERROR_UNAUTHORIZED,
 } = require('../errors');
 
 const getUsers = async (req, res) => {
@@ -38,20 +40,27 @@ const getUser = async (req, res) => {
   }
 };
 
-const createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    return res.status(201).send(user);
-  } catch (e) {
-    if (e.name === 'ValidationError') {
-      return res.status(ERROR_INACCURATE_DATA).send({
-        message: 'Переданы некорректные данные при создании пользователя',
+const createUser = (req, res) => {
+  const {
+    email, password, name, about, avatar,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        email, password: hash, name, about, avatar,
       });
-    }
-    return res
-      .status(ERROR_INTERNAL_SERVER)
-      .send({ message: 'На сервере произошла ошибка' });
-  }
+    })
+    .then((user) => res.status(201).send(user))
+    .catch((e) => {
+      if (e.name === 'ValidationError') {
+        return res.status(ERROR_INACCURATE_DATA).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
+      }
+      return res
+        .status(ERROR_INTERNAL_SERVER)
+        .send({ message: 'На сервере произошла ошибка' });
+    });
 };
 
 const patchUser = async (req, res) => {
@@ -106,6 +115,27 @@ const patchUserAvatar = async (req, res) => {
       .status(ERROR_INTERNAL_SERVER)
       .send({ message: 'На сервере произошла ошибка' });
   }
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильная почта или пароль'));
+      }
+      return res.send({ message: 'Всё верно!' });
+    })
+    .catch((err) => {
+      res.status(ERROR_UNAUTHORIZED).send({ message: err.message });
+    });
 };
 
 module.exports = {
